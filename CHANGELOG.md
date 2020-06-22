2087adbb7862ace89e96d4eeab845cfb5b63902d
Conan Theobald <github@conans.co.uk>
2020-06-22 17:38:52 +0100

    Compatibility with projects using Rollup, upgrade deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [2.3.4] - 2020-06-22
+### Fixed
+- Compatibility with projects using Rollup
+
 ### Updated
 - Updated babel, documentation, eslint, rollup
 
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict'
 
-if (process.env.NODE_ENV !== 'production') {
+// Added for compatibility with a project using Rollup
+let proc
+try {
+  proc = process
+} catch (e) {
+  proc = { env: { NODE_ENV: 'development' } }
+}
+
+if (proc.env.NODE_ENV !== 'production') {
   module.exports = require('./dist/cjs/statebot.dev.js')
 } else {
   module.exports = require('./dist/cjs/statebot.min.js')

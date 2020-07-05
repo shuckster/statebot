5cd3f91496795cdac05a3bc7a2b72b6b57f9b532
Conan Theobald <github@conans.co.uk>
2020-07-05 15:13:41 +0100

    Got started on some basic tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  coverageProvider: "v8",
+  roots: [
+    "<rootDir>/tests"
+  ],
+  testEnvironment: "node",
+};
---
+++ tests/parsing.test.js
@@ -0,0 +1,145 @@
+
+const { Statebot } = require('../src/statebot')
+const { decomposeChart } = require('../src/parsing')
+const { routeIsPossible } = require('../src/assertions')
+
+const SEMANTICALLY_IDENTICAL_CHARTS = [
+  {
+    description: 'Minimal',
+    charts: [
+      `idle -> done`,
+      `idle ->
+       done`,
+      `idle -> // Comment
+       done`,
+    ],
+    decomposeTo: {
+      states: ['idle', 'done'],
+      routes: ['idle->done'],
+      transitions: [['idle', 'done']]
+    },
+    assertions: {
+      possibleRoutes: ['idle->done'],
+      impossibleRoutes: ['done->idle']
+    }
+  },
+  {
+    description: 'Promise-like',
+    charts: [
+      `
+        idle -> pending -> (rejected | resolved) -> finished
+      `,
+      `
+        idle -> pending
+        pending -> (rejected | resolved)
+        (rejected | resolved) -> finished
+      `,
+      `
+        idle -> pending
+        pending -> rejected
+        pending -> resolved
+        rejected -> finished
+        resolved -> finished
+      `,
+      `
+        rejected -> finished
+        resolved -> finished
+        idle -> pending
+        pending -> (rejected |   // Comment
+        resolved)
+      `,
+      `
+        (rejected | resolved) -> // Comment
+          finished
+        pending ->               // Comment
+          (rejected | resolved)  // Comment
+        idle ->
+          pending
+      `
+    ],
+    decomposeTo: {
+      states: [
+        'idle',
+        'pending',
+        'rejected',
+        'resolved',
+        'finished'
+      ],
+      routes: [
+        'idle->pending',
+        'pending->rejected',
+        'pending->resolved',
+        'rejected->finished',
+        'resolved->finished'
+      ],
+      transitions: [
+        ['idle', 'pending'],
+        ['pending', 'rejected'],
+        ['pending', 'resolved'],
+        ['rejected', 'finished'],
+        ['resolved', 'finished']
+      ]
+    },
+    assertions: {
+      possibleRoutes: [
+        'idle -> pending -> resolved -> finished',
+        'idle -> pending -> rejected -> finished'
+      ],
+      impossibleRoutes: [
+        'idle -> resolved',
+        'idle -> rejected',
+        'idle -> finished',
+        'pending -> finished'
+      ]
+    }
+  }
+]
+
+SEMANTICALLY_IDENTICAL_CHARTS.forEach(chartTests => {
+  const { description, charts, decomposeTo, assertions } = chartTests
+
+  charts.forEach(chartToTest => {
+    const { states, routes, transitions } = decomposeChart(chartToTest)
+
+    test(`${description} :: Semantically identical charts produce the same states/routes\n${chartToTest}\n`, () => {
+
+      // States
+      expect(states.length)
+        .toEqual(decomposeTo.states.length)
+      expect(states)
+        .toEqual(expect.arrayContaining(decomposeTo.states))
+
+      // Routes
+      expect(routes.length)
+        .toEqual(decomposeTo.routes.length)
+      expect(routes)
+        .toEqual(expect.arrayContaining(decomposeTo.routes))
+
+      // Transitions
+      expect(transitions.length)
+        .toEqual(decomposeTo.transitions.length)
+      expect(transitions)
+        .toEqual(expect.arrayContaining(decomposeTo.transitions))
+
+    })
+
+    if (!assertions) {
+      return
+    }
+
+    const bot = Statebot(description, { chart: chartToTest })
+    const { possibleRoutes, impossibleRoutes } = assertions
+
+    possibleRoutes.forEach(route => {
+      test(`${description} :: route possible :: ${route}`, () => {
+        expect(true).toEqual(routeIsPossible(bot, route))
+      })
+    })
+
+    impossibleRoutes.forEach(route => {
+      test(`${description} :: route not possible :: ${route}`, () => {
+        expect(false).toEqual(routeIsPossible(bot, route))
+      })
+    })
+  })
+})
